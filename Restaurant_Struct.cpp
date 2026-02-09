6dad18e6c0d540504389f0070187942cdeffdde8
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-02-08 16:30:45 -0800

    10 min update + taking a break

--- Restaurant_Struct.cpp
+++ Restaurant_Struct.cpp
@@ -7,12 +7,29 @@ struct Restaurant{
     string address;
     double avg_price;
     int capacity;
-    float rating;
+    bool has_restroom;
     char sanitary_grade;
 };
 
+Restaurant createRestautant(){
+    Restaurant temp;
+
+    cout << "Enter restaurant address: ";
+    getline(cin, temp.address);
+    cout << "Enter average price: ";
+    cin >> temp.avg_price;
+    cout << "Enter capacity: ";
+    cin >> temp.capacity;
+    cout << "Has restrooms?(true/false): ";
+    cin >> temp.has_restroom;
+    cout << "Enter sanitary grade (A-F): ";
+    cin >> temp.sanitary_grade;
+
+    return temp;
+}
 
 int main(){
 
+    Restaurant res1 = createRestautant();
     return 0;
 }

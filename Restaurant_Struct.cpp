5fda9f4f5bd0b357698b36cf2bdd441ff1c3ed5c
Amogh Nadakudhiti <amogh.nad@gmail.com>
2026-02-08 16:19:33 -0800

    10 min update

--- Restaurant_Struct.cpp
+++ Restaurant_Struct.cpp
@@ -3,3 +3,16 @@
 #include <string>
 using namespace std;
 
+struct Restaurant{
+    string address;
+    double avg_price;
+    int capacity;
+    float rating;
+    char sanitary_grade;
+};
+
+
+int main(){
+
+    return 0;
+}
